8f823e84c5a0c160e59736cd13a3bb1e005fa623
wesley-paiva-vega <wesley.snybool.dev@gmail.com>
2023-10-02 14:59:19 -0300

    logic of checkbox finaly

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,94 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
+
 import useWebSocket, { ReadyState } from "react-use-websocket";
+
 import axios from "axios";
 
-function App() {
-  const WS_URL = "wss://stream.binance.com:9443/ws/";
-  const currency = "btcusdt@trade";
+import { useId } from "react";
 
-  const { lastJsonMessage } = useWebSocket<{ p: string }>(
-    `${WS_URL}${currency}`,
+import * as S from "./styles";
+
+function App() {
+  const [symbols, setSymbols] = useState<string[]>([]);
+
+  const WS_URL = "wss://stream.binance.com:9443/ws/";
+  const currencyPairs = ["btcusdt@trade", "ethusdt@trade", "bnbusdt@trade"];
+
+  enum SymbolsEnum {
+    "BTCUSDT" = "ethusdt@trade",
+    "ETHUSDT" = "ethusdt@trade",
+    "BNBUSDT" = "bnbusdt@trade",
+  }
+
+  const criptoSymbols = [
     {
-      onOpen: () => console.log("Conectado"),
-      onError: () => console.log("Error"),
-      shouldReconnect: () => true,
-      reconnectInterval: 1000,
-      onMessage: () => console.log(),
+      code: "BTCUSDT",
+      id: 1,
+    },
+    {
+      code: "ETHUSDT",
+      id: 2,
+    },
+    {
+      code: "BNBUSDT",
+      id: 3,
+    },
+  ];
+
+  /*   currencyPairs.forEach((currency) => {
+    const { lastJsonMessage } = useWebSocket<{ p: string }>(
+      `${WS_URL}${currency}`,
+      {
+        onOpen: () => console.log(`Conectado a ${currency}`),
+        onError: () => console.log(`Erro ao conectar a ${currency}`),
+        shouldReconnect: () => true,
+        reconnectInterval: 1000,
+        onMessage: () => {
+          console.log(lastJsonMessage);
+        },
+      }
+    );
+  }); */
+
+  const handleAddSymbol = (flag: string) => {
+    if (!symbols.includes(flag)) {
+      setSymbols((prev) => [...prev, flag]);
+    } else {
+      let newState = symbols.filter((item) => item !== flag);
+      setSymbols(newState);
     }
-  );
+  };
+
+  const handleAddCripto = () => {};
 
   useEffect(() => {
-    console.log(lastJsonMessage);
-  }, [lastJsonMessage]);
+    console.log(symbols);
+  }, [symbols]);
 
   return (
     <>
-      <span>ETC: {lastJsonMessage?.p}</span>
+      <span>ETC: {"lastJsonMessage?.p"}</span>
 
-      <span>BTC: {lastJsonMessage?.p}</span>
+      <span>BTC: {"lastJsonMessage?.p"}</span>
+
+      <span onClick={handleAddCripto}>Adicionar Cripto</span>
+
+      <S.BoxSelector>
+        {criptoSymbols.map((item) => {
+          return (
+            <div key={`${item.id}`}>
+              <input
+                type="checkbox"
+                defaultChecked={false}
+                id={`${item.id}`}
+                name="item.code"
+                onClick={() => handleAddSymbol(item.code)}
+              />
+              <span>{item.code}</span>
+            </div>
+          );
+        })}
+      </S.BoxSelector>
     </>
   );
 }
---
+++ src/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const BoxSelector = styled.div`
+  width: 250px;
+  height: 450px;
+  background-color: lightcyan;
+`;

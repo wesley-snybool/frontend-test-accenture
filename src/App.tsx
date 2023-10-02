a266445ccbf1918db2dd4df2cebaf8ebb7cf954f
wesley-paiva-vega <wesley.snybool.dev@gmail.com>
2023-10-02 14:05:12 -0300

    config conection finaly

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import "./App.css";
+import { useEffect } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import axios from "axios";
 
 function App() {
+  const WS_URL = "wss://stream.binance.com:9443/ws/";
+  const currency = "btcusdt@trade";
+
+  const { lastJsonMessage } = useWebSocket<{ p: string }>(
+    `${WS_URL}${currency}`,
+    {
+      onOpen: () => console.log("Conectado"),
+      onError: () => console.log("Error"),
+      shouldReconnect: () => true,
+      reconnectInterval: 1000,
+      onMessage: () => console.log(),
+    }
+  );
+
+  useEffect(() => {
+    console.log(lastJsonMessage);
+  }, [lastJsonMessage]);
+
   return (
     <>
-      <div>Hello</div>
+      <span>ETC: {lastJsonMessage?.p}</span>
+
+      <span>BTC: {lastJsonMessage?.p}</span>
     </>
   );
 }
